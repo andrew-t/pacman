c395f29c173c55b567309e6c8ec1e7ba8c922aa3
Andrew <andrew@andrewt.net>
2020-05-07 17:36:21 +0100

    wall types

--- scripts/Map.js
+++ scripts/Map.js
@@ -22,105 +22,113 @@ const allBlockClasses = [
 	'ghost-door',
 ];
 
-// This version has the correct ghost door...
-// const map = `
-// ############################
-// #••••••••••••##••••••••••••#
-// #•####•#####•##•#####•####•#
-// #*####•#####•##•#####•####*#
-// #•####•#####•##•#####•####•#
-// #••••••••••••••••••••••••••#
-// #•####•##•########•##•####•#
-// #•####•##•########•##•####•#
-// #••••••##••••##••••##••••••#
-// ######•#####-##-#####•######
-// -----#•#####-##-#####•#-----
-// -----#•##----------##•#-----
-// -----#•##-###==###-##•#-----
-// ######•##-###==###-##•######
-// ------•##-#------#-##•------
-// ######•##-########-##•######
-// -----#•##-########-##•#-----
-// -----#•##----------##•#-----
-// -----#•##-########-##•#-----
-// ######•##-########-##•######
-// #••••••••••••##••••••••••••#
-// #•####•#####•##•#####•####•#
-// #•####•#####•##•#####•####•#
-// #*••##•••••••--•••••••##••*#
-// ###•##•##•########•##•##•###
-// ###•##•##•########•##•##•###
-// #••••••##••••##••••##••••••#
-// #•##########•##•##########•#
-// #•##########•##•##########•#
-// #••••••••••••••••••••••••••#
-// ############################
-// `;
+const blockTypes = {
+	'•': BlockTypes.Dot,
+	'-': BlockTypes.Nothing,
+	'#': BlockTypes.Wall,
+	'.': BlockTypes.Wall,
+	',': BlockTypes.Wall,
+	'"': BlockTypes.Wall,
+	"'": BlockTypes.Wall,
+	'r': BlockTypes.Wall,
+	'7': BlockTypes.Wall,
+	'L': BlockTypes.Wall,
+	'J': BlockTypes.Wall,
+	'_': BlockTypes.Wall,
+	'^': BlockTypes.Wall,
+	'[': BlockTypes.Wall,
+	']': BlockTypes.Wall,
+	'~': BlockTypes.Wall,
+	'*': BlockTypes.PowerPill,
+	'=': BlockTypes.GhostDoor
+};
 
-// ...but this version's easier to code for
+const wallClasses = {
+	'#': 'internal-wall',
+	'.': 'top-left-convex-wall',
+	',': 'top-right-convex-wall',
+	'"': 'bottom-left-convex-wall',
+	"'": 'bottom-right-convex-wall',
+	'r': 'top-left-concave-wall',
+	'7': 'top-right-concave-wall',
+	'L': 'bottom-left-concave-wall',
+	'J': 'bottom-right-concave-wall',
+	'_': 'bottom-wall',
+	'^': 'top-wall',
+	'[': 'left-wall',
+	']': 'right-wall',
+	'~': 'unstyled-wall',
+	'=': 'unstyled-wall'
+};
+
 const map = `
-############################
-#••••••••••••##••••••••••••#
-#•####•#####•##•#####•####•#
-#*####•#####•##•#####•####*#
-#•####•#####•##•#####•####•#
-#••••••••••••••••••••••••••#
-#•####•##•########•##•####•#
-#•####•##•########•##•####•#
-#••••••##••••##••••##••••••#
-######•#####-##-#####•######
------#•#####-##-#####•#-----
------#•##----------##•#-----
------#•##-##=##=##-##•#-----
-######•##-##=##=##-##•######
-------•##-#------#-##•------
-######•##-########-##•######
------#•##-########-##•#-----
------#•##----------##•#-----
------#•##-########-##•#-----
-######•##-########-##•######
-#••••••••••••##••••••••••••#
-#•####•#####•##•#####•####•#
-#•####•#####•##•#####•####•#
-#*••##•••••••--•••••••##••*#
-###•##•##•########•##•##•###
-###•##•##•########•##•##•###
-#••••••##••••##••••##••••••#
-#•##########•##•##########•#
-#•##########•##•##########•#
-#••••••••••••••••••••••••••#
-############################
+r^^^^^^^^^^^^7r^^^^^^^^^^^^7
+]••••••••••••[]••••••••••••[
+]•.__,•.___,•[]•.___,•.__,•[
+]*[##]•[###]•[]•[###]•[##]*[
+]•"^^'•"^^^'•"'•"^^^'•"^^'•[
+]••••••••••••••••••••••••••[
+]•.__,•.,•.______,•.,•.__,•[
+]•"^^'•[]•"^^7r^^'•[]•"^^'•[
+]••••••[]••••[]••••[]••••••[
+L____,•[L__,-[]-.__J]•.____J
+#####]•[r^^'-"'-"^^7]•[#####
+#####]•[]----------[]•[#####
+#####]•[]-~~=~~=~~-[]•[#####
+^^^^^'•[]-~~=~~=~~-[]•"^^^^^
+------•[]-~------~-[]•------
+_____,•[]-~~~~~~~~-[]•._____
+#####]•[]-~~~~~~~~-[]•[#####
+#####]•[]----------[]•[#####
+#####]•[]-.______,-[]•[#####
+^^^^^'•"'-"^^7r^^'-"'•"^^^^7
+]••••••••••••[]••••••••••••[
+]•.__,•.___,•[]•.___,•.__,•[
+]•"^7]•"^^^'•[]•"^^^'•]r^'•[
+]*••[]•••••••--•••••••[]••*[
+L_,•[]•.,•.______,•.,•[]•._J
+r^'•"'•[]•"^^7r^^'•[]•"'•"^7
+]••••••[]••••[]••••[]••••••[
+]•.____JL__,•[]•.__JL____,•[
+]•"^^^^^^^^'•"'•"^^^^^^^^'•[
+]••••••••••••••••••••••••••[
+L__________________________J
 `;
 
 class Map {
 	constructor(txt) {
-		const blockTypes = {
-			'•': BlockTypes.Dot,
-			'-': BlockTypes.Nothing,
-			'#': BlockTypes.Wall,
-			'*': BlockTypes.PowerPill,
-			'=': BlockTypes.GhostDoor
-		};
 
-		this.tiles = map
-			.split('\n')
-			.filter(x => x)
-			.map(row => row.split('')
-				.map(c => blockTypes[c]));
-		this.width = this.tiles[0].length;
-		this.height = this.tiles.length;
-
+		// Build elements and tiles object in one pass:
+		this.tiles = [];
+		this.tileEls = [];
 		const mapView = document.getElementById('background');
-		this.tileEls = this.tiles.map(row => {
+		for (const row of map.split('\n').filter(x => x)) {
+			const tilesRow = [];
+			this.tiles.push(tilesRow);
+			const tileElsRow = [];
+			this.tileEls.push(tileElsRow);
 			const tr = document.createElement('tr');
 			mapView.appendChild(tr);
-			return row.map(cell => {
+			for (const c of row.split('')) {
+				const type = blockTypes[c];
+				tilesRow.push(type);
 				const td = document.createElement('td');
-				td.classList.add(`dot-type-${-~(Math.random() * 8)}`);
+				switch (type) {
+					case BlockTypes.Dot:
+						td.classList.add(`dot-type-${-~(Math.random() * 8)}`);
+						break;
+					case BlockTypes.Wall:
+					case BlockTypes.GhostDoor:
+						td.classList.add(wallClasses[c]);
+						break;
+				}
 				tr.appendChild(td);
-				return td;
-			});
-		});
+				tileElsRow.push(td);
+			}
+		}
+
+		this.width = this.tiles[0].length;
+		this.height = this.tiles.length;
 
 		this.draw();
 	}
